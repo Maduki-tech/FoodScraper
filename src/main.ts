13577f4062da8dd5f2b0816e168c8347e03a8e89
Maduki-tech <maduki.business@gmail.com>
2022-06-02 21:04:10 +0200

    added Dynamic Path

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,75 @@
 import axios from "axios";
 import cheerio from "cheerio";
-import pretty from "pretty";
+import fs from "fs";
 
-const URL =
-    "https://www.chefkoch.de/rezepte/1367011241524338/Der-weltbeste-Kraeuterquark.html";
+const URL = process.argv.slice(2)[0];
+let dishName:string;
 
 interface List {
-	menge: string | null
-	zutat: string
-
+    menge: string | null;
+    zutat: string;
 }
 
 async function getDate() {
     try {
         const { data } = await axios.get(URL);
         const $ = cheerio.load(data);
+		dishName = $("h1").text().replace(/ /g, '_')
         const ingredients = $(".table-header tbody tr ");
         const table: object[] = [];
 
+
         ingredients.each((_, ele) => {
-			const food = <List>{}
+            const food = <List>{};
 
-			let menge = $(ele).children(".td-left").children("span").text();
-			// weird format thats why this slicing
-			let first = menge?.trim().split(' ')[0];
-			let last = menge?.trim().split(' ').pop();
+            let menge = $(ele).children(".td-left").children("span").text();
+            // weird format thats why this slicing
+            let first = menge?.trim().split(" ")[0];
+            let last = menge?.trim().split(" ").pop();
 
-            food.menge = first + ' ' + last
-            food.zutat = $(ele).children(".td-right").children("span").text();
+            food.menge = first + " " + last;
+
+            food.zutat = $(ele)
+                .children(".td-right")
+                .children("span")
+                .text()
+                .replace(/,/g, ".");
 
             table.push(food);
         });
-		console.table(table)
+		console.log(dishName)
+        return table;
     } catch (err) {
         console.error(err);
     }
 }
 
-getDate()
+const convertToCSV = (table: any) => {
+    const CSVdate = ConvertToCSV(table);
+    fs.writeFileSync(`/Users/davidschluter/Documents/essen/${dishName}.csv`, CSVdate);
+};
 
+function ConvertToCSV(objArray: any) {
+    var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
+    var str = "";
+
+    for (var i = 0; i < array.length; i++) {
+        var line = "";
+        for (var index in array[i]) {
+            if (line != "") line += ",";
+
+            line += array[i][index];
+        }
+
+        str += line + "\r\n";
+    }
+
+    return str;
+}
+
+const main = async () => {
+    const table = await getDate();
+    convertToCSV(table);
+};
+
+main();
